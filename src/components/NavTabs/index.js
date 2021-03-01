e1283508acf336acc6326965760622ab9e8079b2
Suda Nimh <rslepejian@gmail.com>
2021-03-01 00:21:44 -0800

    added react-portfolio to links

--- src/components/NavTabs/index.js
+++ src/components/NavTabs/index.js
@@ -5,14 +5,14 @@ function NavTabs() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">Home</a>
+        <a className="navbar-brand" href="/react-portfolio">Home</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <a className="nav-link" href="/about">About</a>
-            <a className="nav-link" href="/projects">Projects</a>
+            <a className="nav-link" href="react-portfolio/about">About</a>
+            <a className="nav-link" href="react-portfolio/projects">Projects</a>
           </div>
         </div>
       </div>
