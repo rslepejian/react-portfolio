8622a284fb248ab2969bd7be1476a98c3e453ec6
Suda Nimh <rslepejian@gmail.com>
2021-03-01 10:20:28 -0800

    fixed routes

--- src/components/NavTabs/index.js
+++ src/components/NavTabs/index.js
@@ -11,8 +11,8 @@ function NavTabs() {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <a className="nav-link" href="react-portfolio/about">About</a>
-            <a className="nav-link" href="react-portfolio/projects">Projects</a>
+            <a className="nav-link" href="/react-portfolio/about">About</a>
+            <a className="nav-link" href="/react-portfolio/projects">Projects</a>
           </div>
         </div>
       </div>
