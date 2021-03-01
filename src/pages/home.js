d176a1efdf3713120c9d8e758e7bbdc85c03ec50
Suda Nimh <rslepejian@gmail.com>
2021-03-01 15:09:31 -0800

    made pdf resume downloadable

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import resume from "../images/Raffi_Lepejian_Resume.pdf";
 
 function Home() {
   return (
@@ -11,7 +12,7 @@ function Home() {
             <li><a href="https://github.com/rslepejian" id="github" target="_blank">Github</a></li>
             <li><a href="https://www.linkedin.com/in/raffi-lepejian-071876153/" id="linkedin" target="_blank">Linkedin</a>
             </li>
-            <li><a href="images/Raffi_Lepejian_Resume.pdf" id="resume" target="_blank" download>Resume (Download)</a></li>
+            <li><a href= {resume} id="resume" target="_blank" download>Resume (Download)</a></li>
           </ul>
         </div>
       </div>
